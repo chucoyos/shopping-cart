62bce33e583af30149b654c9d18dc964b101c386
chucoyos <facturasmanzanillo@hotmail.com>
2021-08-29 08:59:58 -0500

    add data

--- app/controllers/item.js
+++ app/controllers/item.js
@@ -3,12 +3,10 @@ import { tracked } from '@glimmer/tracking';
 import { action } from '@ember/object';
 
 export default class ItemController extends Controller {
-  @tracked color = 'red';
+  @tracked color = this.model.colors[0].color;
 
   get productImage() {
-    if (this.color == 'red') {
-      return '/assets/images/beats-solo-red.png';
-    } else return '/assets/images/beats-solo-black.png';
+    return this.model.colors.find(({ color }) => color === this.color).image;
   }
 
   @action
